f76457c8445ab32771df32a497e49e69ff826ef3
SimoneChiodo <simochi.005@gmail.com>
2025-04-30 17:28:05 +0200

    Fix Header, index.html

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/DiscountController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/DiscountController.java
@@ -1,7 +1,6 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controllers;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Discount;
-import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.DiscountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
